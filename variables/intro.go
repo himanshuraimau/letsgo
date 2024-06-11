156c5118cd9b6b41fa86104564444c36c201948c
Himanshu Rai <himanshuraimau9@gmail.com>
2024-06-11 16:39:20 +0530

    add variables and datatypes

---
+++ variables/intro.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string = "John"                  
+
+    var age int = 30
+    var isStudent bool = true
+	         //   name := "John"
+			   //   age := 30
+            //   isStudent := true
+
+    fmt.Println(name, age, isStudent)
+}
