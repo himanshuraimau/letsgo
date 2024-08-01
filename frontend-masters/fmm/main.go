a4e8f3d92cdc7b34575d2fa31673a14fc0d864d1
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-01 15:18:22 +0530

    add templates

--- frontend-masters/fmm/main.go
+++ frontend-masters/fmm/main.go
@@ -1,16 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
 func handlehello(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello World"))
 }
+
+func handleTemplate(w http.ResponseWriter, r *http.Request){
+	html,err:=template.ParseFiles("templates/index.tmpl")
+    
+	if err!=nil{
+	    w.Write([]byte("Error parsing template")) 
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+	}
+	html.Execute(w,"TEST DATA") 
+}
+
 
 func main(){
      server:=http.NewServeMux()
 	http.HandleFunc("/hello",handlehello)
    
 	fs :=http.FileServer(http.Dir("./public"))
+	server.Handle("/",fs)
 
 	err:=http.ListenAndServe(":8080",server)
     if err!=nil{
